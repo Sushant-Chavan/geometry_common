97fced83f4d5121b69c2aa3842a77a3ac737c836
Dharmin B <dharmin.bakaraniya@locomotec.com>
2022-02-11 18:06:26 +0100

    [test] Added skeleton for tests

---
+++ test/point_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <geometry_common/point.h>
+
+// Demonstrate some basic assertions.
+TEST(PointTest, constructor) {
+    geometry_common::Point p;
+    EXPECT_EQ(p.x, 0.0f) << "p.x is not zero";
+    EXPECT_EQ(p.y, 0.0f) << "p.x is not zero";
+    EXPECT_EQ(p.z, 0.0f) << "p.x is not zero";
+
+    geometry_common::Point pt(1.0f, 2.0f, 3.0f);
+    EXPECT_EQ(pt.x, 1.0f) << "pt.x is not 1.0f";
+    EXPECT_EQ(pt.y, 2.0f) << "pt.x is not 2.0f";
+    EXPECT_EQ(pt.z, 3.0f) << "pt.x is not 3.0f";
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
