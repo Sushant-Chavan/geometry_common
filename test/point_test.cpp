4efd8bc4188d1ff6ed13e3a41c97d97a59d23152
Dharmin B <dharmin.bakaraniya@locomotec.com>
2022-02-25 18:33:37 +0100

    [test] Added few tests for pose_2d data structure

--- test/point_test.cpp
+++ test/point_test.cpp
@@ -5,13 +5,13 @@ TEST(PointTest, constructor)
 {
     geometry_common::Point p;
     EXPECT_EQ(p.x, 0.0f) << "p.x is not zero";
-    EXPECT_EQ(p.y, 0.0f) << "p.x is not zero";
-    EXPECT_EQ(p.z, 0.0f) << "p.x is not zero";
+    EXPECT_EQ(p.y, 0.0f) << "p.y is not zero";
+    EXPECT_EQ(p.z, 0.0f) << "p.z is not zero";
 
     geometry_common::Point pt(1.0f, 2.0f, 3.0f);
     EXPECT_EQ(pt.x, 1.0f) << "pt.x is not 1.0f";
-    EXPECT_EQ(pt.y, 2.0f) << "pt.x is not 2.0f";
-    EXPECT_EQ(pt.z, 3.0f) << "pt.x is not 3.0f";
+    EXPECT_EQ(pt.y, 2.0f) << "pt.y is not 2.0f";
+    EXPECT_EQ(pt.z, 3.0f) << "pt.z is not 3.0f";
 }
 
 int main(int argc, char **argv)
