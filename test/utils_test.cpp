35d424235b33e232c2ecba36052df131a6c3ce2f
Dharmin B <dharmin.bakaraniya@locomotec.com>
2022-03-07 21:18:19 +0100

    [test][utils] Moved euler to/from quaternion tests to utils from pose_2d

--- test/utils_test.cpp
+++ test/utils_test.cpp
@@ -22,6 +22,84 @@ TEST(UtilsTest, getAngleBetweenPoints)
         << "Angle should be -3pi/4.";
 }
 
+TEST(UtilsTest, getEulerFromQuaternion)
+{
+    float roll, pitch, yaw;
+    geometry_msgs::Quaternion q;
+    q.x = 0.0f;
+    q.y = 0.0f;
+    q.z = 0.0f;
+    q.w = 1.0f;
+    Utils::getEulerFromQuaternion(q.x, q.y, q.z, q.w, roll, pitch, yaw);
+    EXPECT_NEAR(roll, 0.0f, 1e-3f);
+    EXPECT_NEAR(pitch, 0.0f, 1e-3f);
+    EXPECT_NEAR(yaw, 0.0f, 1e-3f);
+
+    q.x = 0.0f;
+    q.y = 0.0f;
+    q.z = 0.4794f;
+    q.w = 0.8776f;
+    Utils::getEulerFromQuaternion(q.x, q.y, q.z, q.w, roll, pitch, yaw);
+    EXPECT_NEAR(roll, 0.0f, 1e-3f);
+    EXPECT_NEAR(pitch, 0.0f, 1e-3f);
+    EXPECT_NEAR(yaw, 1.0f, 1e-3f);
+
+    q.x = 0.0f;
+    q.y = 0.0f;
+    q.z = -0.4794f;
+    q.w = 0.8776f;
+    Utils::getEulerFromQuaternion(q.x, q.y, q.z, q.w, roll, pitch, yaw);
+    EXPECT_NEAR(roll, 0.0f, 1e-3f);
+    EXPECT_NEAR(pitch, 0.0f, 1e-3f);
+    EXPECT_NEAR(yaw, -1.0f, 1e-3f);
+
+    q.x = 0.1675f;
+    q.y = 0.5709f;
+    q.z = 0.1675f;
+    q.w = 0.7861f;
+    Utils::getEulerFromQuaternion(q.x, q.y, q.z, q.w, roll, pitch, yaw);
+    EXPECT_NEAR(roll, 1.0f, 1e-3f);
+    EXPECT_NEAR(pitch, 1.0f, 1e-3f);
+    EXPECT_NEAR(yaw, 1.0f, 1e-3f);
+}
+
+TEST(UtilsTest, getQuaternionFromEuler)
+{
+    float qx, qy, qz, qw;
+    float roll = 0.0f;
+    float pitch = 0.0f;
+    float yaw = 0.0f;
+
+    Utils::getQuaternionFromEuler(roll, pitch, yaw, qx, qy, qz, qw);
+    EXPECT_NEAR(qx, 0.0f, 1e-3f);
+    EXPECT_NEAR(qy, 0.0f, 1e-3f);
+    EXPECT_NEAR(qz, 0.0f, 1e-3f);
+    EXPECT_NEAR(qw, 1.0f, 1e-3f);
+
+    yaw = 1.0f;
+    Utils::getQuaternionFromEuler(roll, pitch, yaw, qx, qy, qz, qw);
+    EXPECT_NEAR(qx, 0.0f, 1e-3f);
+    EXPECT_NEAR(qy, 0.0f, 1e-3f);
+    EXPECT_NEAR(qz, 0.4794f, 1e-3f);
+    EXPECT_NEAR(qw, 0.8776f, 1e-3f);
+
+    yaw = -1.0f;
+    Utils::getQuaternionFromEuler(roll, pitch, yaw, qx, qy, qz, qw);
+    EXPECT_NEAR(qx, 0.0f, 1e-3f);
+    EXPECT_NEAR(qy, 0.0f, 1e-3f);
+    EXPECT_NEAR(qz, -0.4794f, 1e-3f);
+    EXPECT_NEAR(qw, 0.8776f, 1e-3f);
+
+    roll = 1.0f;
+    pitch = 1.0f;
+    yaw = 1.0f;
+    Utils::getQuaternionFromEuler(roll, pitch, yaw, qx, qy, qz, qw);
+    EXPECT_NEAR(qx, 0.1675f, 1e-3f);
+    EXPECT_NEAR(qy, 0.5709f, 1e-3f);
+    EXPECT_NEAR(qz, 0.1675f, 1e-3f);
+    EXPECT_NEAR(qw, 0.7861f, 1e-3f);
+}
+
 int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
